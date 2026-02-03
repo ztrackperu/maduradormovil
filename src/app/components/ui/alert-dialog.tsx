c2aa8cfc68c4f6f1f8b912aa20e373e6c5d487e6
devpablito2023 <devpablito2023@gmail.com>
2026-02-03 15:44:21 -0500

    liebrado 1

--- src/app/components/ui/alert-dialog.tsx
+++ src/app/components/ui/alert-dialog.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 
 import { cn } from "./utils";
-import { buttonVariants } from "./button";
+import { buttonVariants } from "./Button";
 
 function AlertDialog({
   ...props
